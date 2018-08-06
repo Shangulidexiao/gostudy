94d0382f9d8450e3b48fdd0a3203702565a43475
han.jian <916117482@qq.com>
2018-08-06 14:33:46 +0800

    ch7

--- ch7/exec.go
+++ ch7/exec.go
@@ -1,19 +1,5 @@
 package ch7
 
-import (
-	"fmt"
-	"io"
-)
-
 func Exec() {
-	var b ByteConter
-	b.Write([]byte("helloword"))
-	fmt.Fprintf(&b, "hello word:%d", 9)
-	fmt.Println(b)
-
-	var w WrodConter
-	fmt.Fprintf(&w, "Hello world")
-	fmt.Println(w)
-
-	var _ io.Writer = &b
+	ivalue()
 }
---
+++ ch7/ivalue.go
@@ -0,0 +1,21 @@
+package ch7
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func ivalue() {
+
+	var w io.Writer //接口值由两部分组成：接口类型 接口值 w：type：nil value:nil
+	w = os.Stdout   // w: type:*os.File value: 指向标准输出的指针
+	w.Write([]byte("hello world"))
+	w = new(bytes.Buffer) // w: type:*bytes.Buffer value: 指向
+	w = nil
+
+	var x interface{} = []int{1, 2, 3, 4}
+	x = []string{"a", "b"}
+	fmt.Printf("%T", x)
+}
